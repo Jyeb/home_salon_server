cb44b54c5b04fe58cf0230153ddd7079f14aaf02
PhilHarps <thephilipharper@gmail.com>
2020-02-04 08:54:54 +1000

    test files

--- routes/api/admin.js
+++ routes/api/admin.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const AdminModel = require('../../models/admins')
-const Admin = require('../../models/seeds/Admin')
+const Admin = require('../../models/admins')
 
 router.get('/', (req, res) => {
   Admin.then(doc => res.send(doc)).catch(err => {
--- test/adminTest.js
+++ test/adminTest.js
@@ -1,11 +1,10 @@
 const chai = require("chai");
-// chai.use(require('chai-json-schema'));
+chai.use(require('chai-json-schema'));
 const assert = require("chai").assert;
 const chaiHttp = ("chai-http");
 const app = require("../server")
 
+
 describe("AdminModel", function(){
-    it("adminModel should have a user", function(){
-        assert.equal(adminModel(), "user");
-    });
+    
 });
