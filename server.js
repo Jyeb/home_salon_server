9eee72097a494c0651eff5c99ac237e8f00e3f13
PhilHarps <thephilipharper@gmail.com>
2020-02-04 12:13:29 +1000

    Initial tests

--- server.js
+++ server.js
@@ -30,3 +30,5 @@ if (process.env.NODE_ENV == 'production') {
 }
 // start server on port PORT
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
+module.exports = app
--- test/adminTest.js
+++ test/adminTest.js
@@ -1,10 +1,24 @@
 const chai = require("chai");
-chai.use(require('chai-json-schema'));
-const assert = require("chai").assert;
-const chaiHttp = ("chai-http");
+// chai.use(require('chai-json-schema'));
+// const assert = require("chai").assert;
+const jwt = require('jsonwebtoken')
+const chaiHttp = require("chai-http");
 const app = require("../server")
+const expect = chai.expect;
 
+chai.use(chaiHttp);
 
-describe("AdminModel", function(){
-    
-});
+describe("Admin Routes", () => {
+    it('Creates an admin user', done => {
+        chai
+            .request(app)
+            .get('/api/admin')
+            .end(function (err, res) {
+                expect(err).to.be.null;
+                expect(res).to.have.status(200);
+                // decode the JWT token in res
+                // expect payload to contain key 'admin'
+                done()
+             });        
+    })
+})
