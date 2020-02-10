ae25812a3df71ddfd73d065faa0b7a3c57dc1755
Jye Bussa <bussajye@gmail.com>
2020-02-10 22:06:08 +1000

    dealform

--- home_salon_client/src/Components/pages/deals/DealForm.js
+++ home_salon_client/src/Components/pages/deals/DealForm.js
@@ -2,55 +2,58 @@ import React, { useState, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { addDeal } from '../../../actions/deal'
-
+import { makeStyles } from '@material-ui/core/styles'
+import { Button } from '@material-ui/core'
+import TextField from '@material-ui/core/TextField'
 const DealForm = ({ addDeal }) => {
-  const [formData, setFormData] =  useState ({
+  const [formData, setFormData] = useState({
     dealname: '',
     price: '',
     services: ''
-  });
+  })
 
-  const { dealname, price, services} = formData;
+  const { dealname, price, services } = formData
 
   const onChange = e =>
-  setFormData({ ...formData, [e.target.name]: e.target.value })
-  
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+
   const onSubmit = async e => {
-    e.preventDefault();
+    e.preventDefault()
     addDeal(formData)
-  };
+  }
 
   return (
-  
     <Fragment>
-      <p>
-        Make Deal
-      </p>
+      <p>Make Deal</p>
       <form onSubmit={e => onSubmit(e)}>
-        <input
-        type='dealname'
-        placeholder='Deal Name'
-        name='dealname'
-        value={dealname}
-        onChange={e => onChange(e)}
-        required
+        <TextField
+          id='standard-basic'
+          type='dealname'
+          placeholder='Deal Name'
+          name='dealname'
+          value={dealname}
+          onChange={e => onChange(e)}
+          required
         />
-        <input
-        type='price'
-        placeholder='price'
-        name='price'
-        value={price}
-        onChange={e => onChange(e)}
+        <TextField
+          id='standard-basic'
+          type='price'
+          placeholder='price'
+          name='price'
+          value={price}
+          onChange={e => onChange(e)}
         />
-        <input
-        type='services'
-        placeholder='Service'
-        name='services'
-        value={services}
-        onChange={e => onChange(e)}
+        <TextField
+          id='standard-basic'
+          type='services'
+          placeholder='Service'
+          name='services'
+          value={services}
+          onChange={e => onChange(e)}
         />
-        <input type='submit' value='Create'
-        />
+        <Button variant='contained' type='submit'>
+          Create
+        </Button>
       </form>
     </Fragment>
   )
