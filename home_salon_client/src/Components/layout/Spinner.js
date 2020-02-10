b5b886ad76107dc4747e1ccd5d067dee104c1976
Jye Bussa <bussajye@gmail.com>
2020-02-10 22:31:55 +1000

    styling

--- home_salon_client/src/Components/layout/Spinner.js
+++ home_salon_client/src/Components/layout/Spinner.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import spinner from './spinner.gif';
+import React from 'react'
+import spinner from './spinner.gif'
 
-export default () => (
-  <h1>loading</h1>
-);
+export default () => <h1>loading...</h1>
--- home_salon_client/src/Components/pages/deals/DealForm.js
+++ home_salon_client/src/Components/pages/deals/DealForm.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { addDeal } from '../../../actions/deal'
 import { makeStyles } from '@material-ui/core/styles'
-import { Button } from '@material-ui/core'
+import { Button, FormControl } from '@material-ui/core'
 import TextField from '@material-ui/core/TextField'
+import '../../../stylesheets/DealForm.scss'
 const DealForm = ({ addDeal }) => {
   const [formData, setFormData] = useState({
     dealname: '',
@@ -21,13 +22,20 @@ const DealForm = ({ addDeal }) => {
     e.preventDefault()
     addDeal(formData)
   }
-
+  const useStyles = makeStyles(theme => ({
+    root: {
+      width: '50vw',
+      marginLeft: '25vw',
+      marginTop: '10vh'
+    }
+  }))
+  const classes = useStyles()
   return (
     <Fragment>
       <p>Make Deal</p>
-      <form onSubmit={e => onSubmit(e)}>
+      <FormControl className={classes.root} onSubmit={e => onSubmit(e)}>
         <TextField
-          id='standard-basic'
+          id='standard-full-width'
           type='dealname'
           placeholder='Deal Name'
           name='dealname'
@@ -35,26 +43,30 @@ const DealForm = ({ addDeal }) => {
           onChange={e => onChange(e)}
           required
         />
+        <br />
         <TextField
-          id='standard-basic'
+          id='standard-full-width'
           type='price'
           placeholder='price'
           name='price'
           value={price}
           onChange={e => onChange(e)}
         />
+        <br />
         <TextField
-          id='standard-basic'
+          id='standard-full-width'
           type='services'
           placeholder='Service'
           name='services'
           value={services}
           onChange={e => onChange(e)}
         />
+        <br />
+        <br />
         <Button variant='contained' type='submit'>
           Create
         </Button>
-      </form>
+      </FormControl>
     </Fragment>
   )
 }
